e29f30d8c61790970c3de1d4aa8a52f5bf26c612
Alefe <alefefreitassantos@gmail.com>
2024-12-05 00:12:30 -0300

    sla mano

---
+++ orcamentoMensal.py
@@ -0,0 +1,7 @@
+despesas = float(input("Digite o total de despesas do mês: "))
+
+if despesas > 3000:
+    print("Atenção! Você ultrapassou o limite do seu orçamento")
+
+else:
+    print("Parabéns! O limite não foi utrapassado")
